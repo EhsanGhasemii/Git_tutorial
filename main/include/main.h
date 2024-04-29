908fe02f150e9e1d9f40ed54327d230c8677a9c1
EhsanGhasemii <ehsanghasemi7998@gmail.com>
2024-04-29 14:45:14 -0400

    Step1: Solving bug 1

---
+++ main/include/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// this is for commit "step1"
+#include <iostream> 
+
+
+
+#endif
---
+++ main/src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/main.h"
+
+int main() { 
+
+	// this is for commit "step1"
+	std::cout << "This is commit: step1" << std::endl; 
+
+	return 0; 
+}
